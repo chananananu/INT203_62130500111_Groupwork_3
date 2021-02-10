b0ee4a855d118370e37c097d2fb197b8b215d245
Chananan Likitsarawit <chananan.anu@mail.kmutt.ac.th>
2021-02-10 16:26:14 +0700

    [Update] index.html main.js

--- 62130500111_Groupwork_3/main.js
+++ 62130500111_Groupwork_3/main.js
@@ -1,20 +1,22 @@
 const app = {
     data() {
         return {
-            cats: [{image: 'image/01.jpg', ig: 'zjiasally (Fan Account)', like: false},
-                    {image: 'image/02.jpg', ig: 'dear.mycats', like: false},
-                    {image: 'image/03.jpg', ig: 'zuzu_and_nala', like: false}
+            cats: [{image: 'image/01.jpg', profile: 'image/01.jpg', ig: 'zjiasally (Fan Account)', like: false},
+                    {image: 'image/02.jpg', profile: 'image/02.jpg', ig: 'dear.mycats', like: false},
+                    {image: 'image/03.jpg', profile: 'image/01.jpg', ig: 'zuzu_and_nala', like: false}
             ],
-            profile: 'image/ig01.jpg'
+            
         }
     },
     methods: {
         clickLike(index){
-            this.cats[index].like = !this.cats[index].like         
+            this.cats[index].like = !this.cats[index].like;      
         }
-        // likeAmount() {
-        //     return this.cats.filter(n => n.like).length;
-        //}
+    },
+    computed: {
+        countPhoto() {
+            return this.cats.length
+        }
     }
 }
 
