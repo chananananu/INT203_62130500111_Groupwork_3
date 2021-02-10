922e8423f47098f6694d7a2790756ff478702ce1
Chananan Likitsarawit <chananan.anu@mail.kmutt.ac.th>
2021-02-10 16:27:23 +0700

    [Update] main.js

--- 62130500111_Groupwork_3/main.js
+++ 62130500111_Groupwork_3/main.js
@@ -1,9 +1,9 @@
 const app = {
     data() {
         return {
-            cats: [{image: 'image/01.jpg', profile: 'image/01.jpg', ig: 'zjiasally (Fan Account)', like: false},
+            cats: [{image: 'image/01.jpg', profile: 'image/01.jpg', ig: 'zjiasally', like: false},
                     {image: 'image/02.jpg', profile: 'image/02.jpg', ig: 'dear.mycats', like: false},
-                    {image: 'image/03.jpg', profile: 'image/01.jpg', ig: 'zuzu_and_nala', like: false}
+                    {image: 'image/03.jpg', profile: 'image/03.jpg', ig: 'zuzu_and_nala', like: false}
             ],
             
         }
